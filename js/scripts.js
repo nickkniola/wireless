81a87722031fb3453bcde35041efa8e7f7efeb9f
Nick Kniola <nickkniola@me.com>
2020-06-10 19:57:54 -0500

    Added modals, with javascript script

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("#submitButton").click(function(){
+        $("#thankModal").modal("show");
+    });
+    
+    $("#buyBasic").click(function(){
+        $("#buyModal").modal("show");
+    });
+    
+
+});
