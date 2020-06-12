499403f33ef9889c2df049733e9f1e87bf3d95f7
Nick Kniola <nickkniola@me.com>
2020-06-12 16:38:22 -0500

    Javascript search functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,22 @@ function total(planCost) {
     return planTotal;
 }
 
-// //If (alltextonpage1, 2 ,or 3) includes (searchedInput) then go to that page
 const homePage = "Welcome to Wireless The only plan which offers free roaming around the world, so you can go anywhere, do anything, without the hassle of roaming fees. Choose from one of our three affordable plans for the future! Get A Free Phone With a purchase of a new plan. Switch Plans And get your first year free!";
 
 const plansPage = "Find Your Plan Wireless is the only mobile provider to include free international roaming. You’ll also get unlimited texts and calls on all of our plans. Basic $29 / mo Free International Roaming 15GB LTE Hotspot per line Unlimited Texting, Calls SD Streaming Elite $39 / mo Free International Roaming 30GB LTE Hotspot per line Unlimited Texting, Calls HD Streaming Unlimited $49 / mo Free International Roaming 100GB LTE Hotspot per line Unlimited Texting, Calls Full HD Streaming";
 
 const supportPage = "Wireless Support First Name Last Name Email Address What device are you using? Type of Issue Description of Issue";
 
-// let searchInp = document.getElementById(".searchInput");
-// let searchBu = document.getElementById(".searchButton")
-// searchBu.addEventListener("click", checkInput);
-
-// function checkInput() {
-//     window.open("https://nickkniola.github.io/wireless/index.html");
-//     if (homePage.includes("to")) {
-//         window.location = "https://nickkniola.github.io/wireless/index.html";
-
-// };
-// };
-
 $(function(){
-    $(".searchButton").on('click', function(){
-        window.location='http://www.google.com/';
-    });
+    $("#searchButton").on('click', function(){
+        if (plansPage.includes(document.querySelector("#searchInput").value)) {
+            $("#searchButton").attr("href", "plans.html") 
+        } else if (homePage.includes(document.querySelector("#searchInput").value)) {
+            $("#searchButton").attr("href", "index.html")
+        } else if (supportPage.includes(document.querySelector("#searchInput").value)) {
+            $("#searchButton").attr("href", "support.html")
+        }
+     });
 
     $("#submitButton").click(function(){
         $("#thankModal").modal("show");
